2ec0f8ba99a802544807b04ae0e5635b11132590
Sheng-Yan, Zhang <yan.code@gmail.com>
2024-07-09 09:49:40 +0800

    feat: modal & copy

--- modal/script.js
+++ modal/script.js
@@ -1,9 +1,18 @@
 const shareButton = document.querySelector(".share-btn")
 const dialog = document.querySelector('dialog')
 const closeButton = document.querySelector('.close-btn')
+const copyButton = document.getElementById("copy-text")
+const urlInput = document.getElementById('copy-link-input')
 shareButton.addEventListener("click", () => {
     dialog.showModal();
 })
 closeButton.addEventListener("click", () => {
     dialog.close();
 })
+copyButton.addEventListener("click", () => {
+    urlInput.select();
+    urlInput.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(urlInput.value);
+    document.getElementById("copy-text").innerText = "Copied!"
+})
+
